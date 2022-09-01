67dac5b0c76e139054ce6adfdda34be71fb8bb08
Abdoulaye GAYE <layegaye001@gmail.com>
2022-09-01 15:36:56 +0000

    first commit

---
+++ src/main/java/com/bank/credit/bankcredit/BankCreditApplication.java
@@ -0,0 +1,13 @@
+package com.bank.credit.bankcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCreditApplication.class, args);
+	}
+
+}
